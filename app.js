4116aa903345d64229bd0e957e4f97d88b6571dc
Shahid Shaikh <rwtc66@gmail.com>
2016-03-14 18:20:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,18 +7,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.get('/',function(req,res) {
+  // Sending our HTML file to browser.
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/submit',function(req,res){
-  console.log(req.body);
-  console.log(req.connection.remoteAddress);
-  var secretKey = "";
+  // g-recaptcha-response is the key that browser will generate upon form submit.
+  // if its blank or null means user has not selected the captcha, so return the error.
+  if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+    return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
+  }
+  // Put your secret key here.
+  var secretKey = "--paste your secret key here--";
+  // req.connection.remoteAddress will provide IP address of connected user.
   var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+  // Hitting GET request to the URL, Google will respond with success or error scenario.
   request(verificationUrl,function(error,response,body) {
-    console.log(body);
-    res.send("Hello");
+    body = JSON.parse(body);
+    // Success will be true or false depending upon captcha validation.
+    if(body.success !== undefined && !body.success) {
+      return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
+    }
+    res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
   });
 });
 
+// This will handle 404 requests.
+app.use("*",function(req,res) {
+  res.status(404).send("404");
+})
+
+// lifting the app on port 3000.
 app.listen(3000);
